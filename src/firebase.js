8e95db9bb6ab8fd623710040e02268e929034ad3
khanmuhammadsafwan-ui <khanmuhammadsafwan@gmail.com>
2026-07-28 00:58:44 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC3kglnrThGLz9T_Xp44fwQQSkRrRVtZRg',
-  authDomain: 'munsiji-app.firebaseapp.com',
-  projectId: 'munsiji-b2a55',
-  storageBucket: 'munsiji-app.firebasestorage.app',
-  messagingSenderId: '1037754771828',
-  appId: '1:1037754771828:web:6ce19e84bf9a93687d6421',
-}
+  apiKey: "AIzaSyC3kglnrThGLz9T_Xp44fwQQSkRrRVtZRg",
+  authDomain: "munsiji-b2a55.firebaseapp.com",
+  projectId: "munsiji-b2a55",
+  storageBucket: "munsiji-b2a55.firebasestorage.app",
+  messagingSenderId: "1037754771828",
+  appId: "1:1037754771828:web:6ce19e84bf9a93687d6421"
+};
 
 const app = initializeApp(firebaseConfig)
 
